1c41ad4d7bb389338eadf57a9861106f9e9c5e15
Adarsh Malviya <adarshmalviya1199@gmail.com>
2024-03-03 14:48:50 +0530

    base url added in constants

--- src/components/admin/AdminProfile.jsx
+++ src/components/admin/AdminProfile.jsx
@@ -1,10 +1,7 @@
-import Modal from "../Modal";
-import LeadTable from "./LeadTable";
-
 const AdminProfile = () => {
   return (
     <div>
-      <Modal title="create lead" children={<LeadTable />} />
+      <div>hello</div>
     </div>
   );
 };
